d1409cb0f9aff574cf97f0ac2d67c03ce543b38d
proyectoContrasenias <proyectogestorcontrasenias@gmail.com>
2025-04-21 18:53:26 +0200

    UPDATE

--- editar.php
+++ editar.php
@@ -13,6 +13,31 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['logout'])) {
     header("Location: index.php?error=Has cerrado sesión correctamente");
     exit();
 }
+// Se conecta a la base de datos y si no muestra el error
+$con = new mysqli("localhost", "proyecto", "proyecto", "keysafe");
+if ($con->connect_error) {
+    die("Error de conexión: " . $con->connect_error);
+}
+// Revisa si se ha pasado correctamente el id del juego desde la página juegos.php y si se ha pasado bien el id, muestra todos los datos del juego
+if(isset($_GET['id'])) {
+    $id_pagina=$_GET['id'];
+    $sentencia="SELECT * FROM contraseñas WHERE id = ?;";
+    $stmt=$con->prepare($sentencia);
+    $stmt->bind_param('i',$id_pagina);
+}
+// Si se pulsa el botón guardar cambios actualiza el juego de la base de datos 
+if (isset($_POST['update_id'])) {
+    $update_id = $_POST['update_id'];
+    $stmtt = $con->prepare("UPDATE contraseñas SET pagina = ?, usuario = ?, contraseña = ? WHERE id = ?");
+    $stmtt->bind_param("sssi", $_POST['nombre'], $_POST['usuario'], $_POST['contrasena'], $update_id);
+    if ($stmtt->execute()) {
+        $stmtt->close();
+        header("Location: dashboard.php");
+        exit();
+    } else {
+        echo "<p>Error al actualizar: " . $stmtt->error . "</p>";
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -35,20 +60,31 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['logout'])) {
     
 
     <main>
-        <form class="form-container" onsubmit="event.preventDefault(); location.href='dashboard.php';">
-            <label for="nombre">Nombre de la Página:</label>
-            <input type="text" id="nombre" name="nombre" value="Facebook" required>
+        <form class="form-container" method="POST">
+        <?php
+                // Recorre la base de datos para mostrar la información del juego
+                if($stmt->execute()){
+                    $result=$stmt->get_result();
+                    while($recorrido=$result->fetch_assoc()){
+                        echo '
+                        <input type="hidden" name="update_id" value="' . $recorrido["id"] . '">
+                        
+                        <label for="nombre">Nombre de la Página:</label>
+                        <input type="text" id="nombre" name="nombre" value="' . $recorrido["pagina"] . '" required>
 
-            <label for="usuario">Usuario:</label>
-            <input type="text" id="usuario" name="usuario" value="usuario@ejemplo.com" required>
+                        <label for="usuario">Usuario:</label>
+                        <input type="text" id="usuario" name="usuario" value="' . $recorrido["usuario"] . '" required>
 
-            <label for="contrasena">Contraseña:</label>
-            <div class="password-container">
-                <input type="password" id="contrasena" name="contrasena" value="12345678" required>
-                <button type="button" class="toggle-password" onclick="togglePassword()">Mostrar</button>
-            </div>
+                        <label for="contrasena">Contraseña:</label>
+                        <div class="password-container">
+                            <input type="password" id="contrasena" name="contrasena" value="' . $recorrido["contraseña"] . '" required>
+                            <button type="button" class="toggle-password" onclick="togglePassword()">Mostrar</button>
+                        </div>
 
-            <button type="submit" class="save-button">Guardar Cambios</button>
+                        <button type="submit" class="save-button">Guardar Cambios</button>';
+                }
+                }
+                ?>
         </form>
     </main>
 
