ae01f05c106efa08ed538a6fae832eb2550e9784
Sergio <sfernandezdiaz2005@gmail.com>
2025-04-21 18:32:31 +0200

    Registro

--- registro.php
+++ registro.php
@@ -1,3 +1,33 @@
+<?php
+$conex = new mysqli("127.0.0.1", "proyecto", "proyecto", "keysafe");
+if ($conex->connect_error) {
+    die("Error de conexión: " . $conex->connect_error);
+}
+
+$mensaje = "";
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $nombrenuevo = $_POST['nombre'];
+    $apellido = $_POST['apellidos'];
+    $usunuevo = $_POST['usuario'];
+    $emailnuevo = $_POST['email'];
+    $connueva = $_POST['contrasena'];
+
+
+
+    $stmt = $conex->prepare('INSERT INTO usuarios (nombre, apellidos, username, contraseña, correo) VALUES(?, ?, ?, ?, ?)');
+    $stmt->bind_param('sssss', $nombrenuevo, $apellido, $usunuevo, $connueva, $emailnuevo);
+
+    if ($stmt->execute()) {
+        $mensaje = "Registro completado correctamente";
+    } else {
+        $mensaje = "Error al crear el usuario: " . $stmt->error;
+    }
+
+    $stmt->close();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -12,7 +42,6 @@
 
             // Compara las contraseñas
             if (contrasena !== repiteContrasena) {
-                // Evita el envío del formulario
                 event.preventDefault();
                 alert("Las contraseñas no coinciden. Por favor, intenta de nuevo.");
             }
@@ -23,7 +52,7 @@
     <div class="container">
         <div class="form-container">
             <h2>Crear cuenta</h2>
-            <form action="index.php" method="POST" onsubmit="validarFormulario(event)">
+            <form action="registro.php" method="POST" onsubmit="validarFormulario(event)">
                 <label for="nombre">Nombre:</label>
                 <input type="text" id="nombre" name="nombre" placeholder="Ingresa tu nombre" required>
 
@@ -40,14 +69,19 @@
                 <input type="password" id="contrasena" name="contrasena" placeholder="Crea una contraseña" required>
 
                 <label for="repite-contrasena">Repite la contraseña:</label>
-                <input type="password" id="repite-contrasena" name="repite-contrasena" placeholder="Repite la contraseña" required>
+                <input type="password" id="repite-contrasena" name="repite-contrasena"
+                    placeholder="Repite la contraseña" required>
 
                 <button type="submit">Registrar</button>
+
+                <?php if ($mensaje): ?>
+                    <p class="success-message"><?php echo $mensaje; ?></p>
+                <?php endif; ?>
             </form>
 
             <p>¿Ya tienes cuenta? <a href="index.php">Inicia sesión aquí</a></p>
         </div>
     </div>
 </body>
-</html>
 
+</html>
