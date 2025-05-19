a14dc342f76a1b8dacfc29e1a0e9c7dfa556d289
Sergio <sfernandezdiaz2005@gmail.com>
2025-05-19 20:51:32 +0200

    Base de Datos mod

--- keysafe.sql
+++ keysafe.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 07-04-2025 a las 17:23:23
+-- Tiempo de generación: 19-05-2025 a las 20:50:04
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -11,28 +11,67 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-DROP DATABASE IF EXISTS keysafe;
-CREATE DATABASE keysafe;
-USE keysafe;
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `keysafe`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `contraseñas`
+--
+
+CREATE TABLE `contraseñas` (
+  `id` int(11) NOT NULL,
+  `pagina` varchar(200) NOT NULL,
+  `usuario` varchar(50) NOT NULL,
+  `contraseña` varchar(50) NOT NULL,
+  `usuario_id` int(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
 
 CREATE TABLE `usuarios` (
-  `id` int(40) NOT NULL AUTO_INCREMENT,
+  `id` int(40) NOT NULL,
   `nombre` varchar(70) NOT NULL,
   `apellidos` varchar(70) NOT NULL,
   `username` varchar(50) NOT NULL,
   `contraseña` varchar(255) NOT NULL,
   `correo` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
+  `google_auth_code` varchar(16) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE `contraseñas` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `pagina` varchar(200) NOT NULL,
-  `usuario` varchar(50) NOT NULL,
-  `contraseña` varchar(50) NOT NULL,
-  `usuario_id` int(40) NOT NULL,
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (`usuario_id`) REFERENCES `usuarios`(`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+
+
+ALTER TABLE `contraseñas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+
+ALTER TABLE `contraseñas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+
+ALTER TABLE `usuarios`
+  MODIFY `id` int(40) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+
+ALTER TABLE `contraseñas`
+  ADD CONSTRAINT `contraseñas_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE;
 COMMIT;
+
